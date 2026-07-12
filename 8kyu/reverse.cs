5e62d5692433eeecfd8d7c75b5162293d0685ce1
Birasa Divine <b.divine@alustudent.com>
2026-07-12 22:39:47 +0200

    Initial setup

---
+++ 8kyu/reverse.cs
@@ -0,0 +1,14 @@
+using System;
+
+// 1. Test your Codewars method here
+int result = Kata.Multiply(5, 4);
+Console.WriteLine($"Result: {result}"); 
+
+// 2. Paste the Codewars class structure below
+public class Kata 
+{
+    public static int Multiply(int a, int b) 
+    {
+        return a * b;
+    }
+}
