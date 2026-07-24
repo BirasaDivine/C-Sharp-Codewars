22289dc535e0677b4e15d48c025022b8de85afc3
Birasa Divine <b.divine@alustudent.com>
2026-07-24 16:02:40 +0200

    Add Program class with Main method to initialize TaskManager and display tasks

---
+++ TaskManager/Program.cs
@@ -0,0 +1,17 @@
+﻿
+namespace TaskManagerApp
+{
+    class Program
+   {
+    static void Main()
+    {   
+        TaskManager manager = new TaskManager();
+        TaskItem task1 = new  TaskItem( 1 ,  "Study Csharp", "Divine" , 4);
+        TaskItem task2 = new  TaskItem( 2 ,  "Enjoyment", "Divine" , 1);
+        manager.AddTask(task1);
+        manager.AddTask(task2);
+        manager.DisplayAll();
+    }
+} 
+}
+
